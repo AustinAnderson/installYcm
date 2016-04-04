8af0cad63c14434b5a8d7fe66ee4e5057e323e67
Austin Anderson <Austin_Anderson1@baylor.edu>
2016-04-03 23:08:46 -0500

    changed vimrc path to literal

--- installYcm.sh
+++ installYcm.sh
@@ -12,11 +12,11 @@ rc=$rc"Plugin 'VundleVim/Vundle.vim'\n"
 rc=$rc"Plugin 'Valloric/YouCompleteMe.git'\n"
 rc=$rc"call vundle#end()            "'"'" required\n"
 rc=$rc"filetype plugin indent on    "'"'" required\n"
-testFile="~/.vimrc"
-cp $testFile tempFile
-echo -e $rc > $testFile;
-cat tempFile >> $testFile;
+cp ~/.vimrc tempFile
+echo -e $rc > ~/.vimrc;
+cat tempFile >> ~/.vimrc;
 rm tempFile
 vim +PluginInstall +qall
 vim "+source %" +PluginInstall +qall
-
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --all
