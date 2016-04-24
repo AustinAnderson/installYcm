237465dd97e5587bc03ba1a57ef925d0f70d778b
Austin Anderson <Austin.David.Anderson1@gmail.com>
2016-04-24 16:10:26 -0500

    added differentiation for 32 and 64 bit, only 32 bit ubuntu works for now

---
+++ installYcm32b.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt-get -y install build-essential cmake
+sudo apt-get -y install python-dev
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+rc="set nocompatible\n"
+rc=$rc"filetype off                  "'"'" required\n"
+rc=$rc"set rtp+=~/.vim/bundle/Vundle.vim\n"
+rc=$rc"call vundle#begin()\n"
+rc=$rc'"'" let Vundle manage Vundle, required\n"
+rc=$rc"Plugin 'VundleVim/Vundle.vim'\n"
+rc=$rc"Plugin 'Valloric/YouCompleteMe.git'\n"
+rc=$rc"call vundle#end()            "'"'" required\n"
+rc=$rc"filetype plugin indent on    "'"'" required\n"
+cp ~/.vimrc tempFile
+echo -e $rc > ~/.vimrc;
+cat tempFile >> ~/.vimrc;
+rm tempFile
+vim +PluginInstall +qall
+sudo apt-get -y install clang lldb libclang-dev
+sudo apt-get -y install mono-xbuild
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer --system-libclang
+wget https://raw.githubusercontent.com/Valloric/ycmd/master/cpp/ycm/.ycm_extra_conf.py
+mv .ycm_extra_conf.py ~
