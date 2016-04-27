980d8d8c425c3c5bbfb80e5d2d6ea518b5f392c1
Austin Anderson <Austin.David.Anderson1@gmail.com>
2016-04-26 22:25:03 -0500

    fixed issue: it was wrong python version

--- setUpVim.sh
+++ setUpVim.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sudo apt-get install -y vim-gtk-py2
-cd ~ && wget cs.ecs.baylor.edu/~andersonau/.vimrc;
+sudo apt-get remove -y --purge vim-*
+sudo apt-get install -y vim-nox-py2
+cd ~;
+mv .vimrc .vimrcOld
+wget cs.ecs.baylor.edu/~andersonau/.vimrc;
 sudo apt-get install g++
 exit 0;
